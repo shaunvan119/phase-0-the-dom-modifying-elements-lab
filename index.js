e73ca28160f3fac4af27de94d13dd86be6f19dbe
shaunvan119 <shaunvan@live.com.au>
2021-03-14 19:44:50 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const header = document.getElementById('header');
+
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+
+document.getElementById('victory');
+
+newHeader.innerHTML="<h1>SHAUN is the champion</h1>";
